df270938b428ad07cfd69858a68226de3b0c1676
Queenshiba <haruka0111.2a@gmail.com>
2021-03-05 23:17:50 +0900

    add textnode

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,15 @@ let wholeConatiner = document.getElementById('whole-calc-conatiner')
 console.log(wholeConatiner)
 let selectNumberBtn = document.getElementsByClassName('selectNumber')
 let numberOfPplValue = document.getElementById('numberOfPpl')
+let setOfEach = document.getElementsByClassName('setOfEach')
+let names = document.getElementsByClassName('name')
 
 function selectNumber() {
     console.log(numberOfPplValue.value)
-    wholeConatiner.style.display = "show"
-   
+    wholeConatiner.style.display = ""
+
+console.log(setOfEach)
+let texrnode = document.createTextNode(setOfEach)
+// wholeConatiner.appendChild(names)
+wholeConatiner.appendChild(texrnode); 
 }
