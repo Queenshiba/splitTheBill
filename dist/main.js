b42fd102793fc836a3a64fcf0502133020784010
Queenshiba <haruka0111.2a@gmail.com>
2021-03-06 23:34:52 +0900

    add elements for a set

--- dist/main.js
+++ dist/main.js
@@ -3,15 +3,43 @@ let wholeConatiner = document.getElementById('whole-calc-conatiner')
 console.log(wholeConatiner)
 let selectNumberBtn = document.getElementsByClassName('selectNumber')
 let numberOfPplValue = document.getElementById('numberOfPpl')
-let setOfEach = document.getElementsByClassName('setOfEach')
-let names = document.getElementsByClassName('name')
+
+
+let inputForName = document.createElement('input')
+inputForName.classList.add('inputForName')
+wholeConatiner.appendChild(inputForName)
+
+let divForPaidItems = document.createElement('div')
+let inputForPaidAmnt = document.createElement('input')
+
+
+let divForAddItems = document.createElement('div')
+divForAddItems.classList.add('divForAddItems')
+let pForAddItems = document.createElement('p')
+pForAddItems.textContent += 'Add items'
+let btnForAddItems = document.createElement('button')
+btnForAddItems.textContent += 'Add'
+divForAddItems.appendChild(pForAddItems)
+divForAddItems.appendChild(btnForAddItems)
+
+divForPaidItems.appendChild(divForAddItems)
+
+let divForResult = document.createElement('div')
+let pForResult = document.createElement('p')
+let textResult = document.createTextNode('Result')
+pForResult.append(textResult)
+divForResult.appendChild(pForResult)
+
+
+wholeConatiner.appendChild(divForAddItems)
+wholeConatiner.appendChild(divForResult)
+
+
 
 function selectNumber() {
     console.log(numberOfPplValue.value)
     wholeConatiner.style.display = ""
 
-console.log(setOfEach)
-let texrnode = document.createTextNode(setOfEach)
-// wholeConatiner.appendChild(names)
-wholeConatiner.appendChild(texrnode); 
+
+
 }
