607be66c5e2fa838d725c7ce1b055904136bd016
Queenshiba <haruka0111.2a@gmail.com>
2021-03-07 19:35:18 +0900

    set the amount to repeat elements

--- dist/main.js
+++ dist/main.js
@@ -4,14 +4,17 @@ console.log(wholeConatiner)
 let selectNumberBtn = document.getElementsByClassName('selectNumber')
 let numberOfPplValue = document.getElementById('numberOfPpl')
 
+let divForSet = document.createElement('div')
+divForSet.classList.add('divForSet')
 
 let inputForName = document.createElement('input')
 inputForName.classList.add('inputForName')
-wholeConatiner.appendChild(inputForName)
+divForSet.appendChild(inputForName)
 
 let divForPaidItems = document.createElement('div')
+divForPaidItems.classList.add('divForPaidItems')
 let inputForPaidAmnt = document.createElement('input')
-
+divForPaidItems.appendChild(inputForPaidAmnt)
 
 let divForAddItems = document.createElement('div')
 divForAddItems.classList.add('divForAddItems')
@@ -27,12 +30,13 @@ divForPaidItems.appendChild(divForAddItems)
 let divForResult = document.createElement('div')
 let pForResult = document.createElement('p')
 let textResult = document.createTextNode('Result')
+divForResult.classList.add('divForResult')
 pForResult.append(textResult)
 divForResult.appendChild(pForResult)
 
-
-wholeConatiner.appendChild(divForAddItems)
-wholeConatiner.appendChild(divForResult)
+divForSet.appendChild(divForPaidItems)
+divForSet.appendChild(divForAddItems)
+divForSet.appendChild(divForResult)
 
 
 
@@ -40,6 +44,10 @@ function selectNumber() {
     console.log(numberOfPplValue.value)
     wholeConatiner.style.display = ""
 
+    for (let i = 0; i < numberOfPplValue.value; i++) {
+        wholeConatiner.appendChild(divForSet.cloneNode(true))
+
+    }
 
 
 }
